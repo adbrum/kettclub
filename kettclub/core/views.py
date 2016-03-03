a261f94572ae89b3161725b5d39def8021f7c89b
adbrum <adbrum@outlook.com>
2016-03-03 14:22:58 +0000

    Add cache in views

--- kettclub/core/views.py
+++ kettclub/core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 from kettclub.subscriptions.forms import SubscriptionForm
 
 
+@cache_page(60)
 def home(request):
     context = {'form': SubscriptionForm}
     return render(request, 'index.html', context)

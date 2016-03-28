13579f078ebb00bc79e66ebe8c325e87ccedd71a
adbrum <adbrum@outlook.com>
2016-03-28 19:03:16 +0100

    Mensagem n. do atleta

--- kettclub/core/views.py
+++ kettclub/core/views.py
@@ -4,6 +4,6 @@ from django.views.decorators.cache import cache_page
 from kettclub.administration.forms import LoginForm
 
 
-@cache_page(60)
+# @cache_page(60)
 def home(request):
     return render(request, 'home.html', {'form': LoginForm})
